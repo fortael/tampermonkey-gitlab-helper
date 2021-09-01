3b80dbb59c33368edf72fdb39f77d5398ad219fb
fortael <fortael@yahoo.com>
2021-09-02 00:06:27 +0500

    add gitlab 13 support and higher

--- mr.meta.js
+++ mr.meta.js
@@ -3,11 +3,12 @@
 // @namespace   Review
 // @description Highlight available merge request
 // @version     0.3.0
+// @homepage    https://github.com/fortael/tampermonkey-gitlab-helper
 // @updateUrl   https://cdn.jsdelivr.net/gh/fortael/tampermonkey-gitlab-helper@latest/mr.meta.js
 // @downloadURL https://cdn.jsdelivr.net/gh/fortael/tampermonkey-gitlab-helper@latest/mr.user.js
 // @match       https://*/*/merge_requests*
-// @require     http://code.jquery.com/jquery-latest.js
 // @require     https://unpkg.com/axios/dist/axios.min.js
 // @require     https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js
 // @grant       none
+// @run-at      document-end
 // ==/UserScript==
